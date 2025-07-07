fb0773e2e4acdeb4d71680d56cfd8aa0c4882b27
nahashon <nashonmwendwa0@gmail.com>
2025-07-07 09:49:49 +0300

    Added authentication middleware to protect dashboard and import routes

--- routes/web.php
+++ routes/web.php
@@ -5,9 +5,14 @@ use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/import-users', [ImportController::class, 'importUsers'])->name('import.users');
-Route::post('/import-sign-ins', [ImportController::class, 'importSignIns'])->name('import.sign_ins');
-Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
-Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
-Route::post('/users', [UserController::class, 'store'])->name('users.store');
-Route::delete('/users/{id}', [UserController::class, 'destroy'])->name('users.destroy');
+Route::middleware('auth')->group(function () {
+    Route::post('/import-users', [ImportController::class, 'importUsers'])->name('import.users');
+    Route::post('/import-sign-ins', [ImportController::class, 'importSignIns'])->name('import.sign_ins');
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+    Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
+    Route::post('/users', [UserController::class, 'store'])->name('users.store');
+    Route::delete('/users/{id}', [UserController::class, 'destroy'])->name('users.destroy');
+    Route::get('/imports', function () {
+        return view('imports.index');
+    })->name('imports.index');
+});
