471404be6b1cc458aaaabc7452218ea1eeedaac8
nahashon <nashonmwendwa0@gmail.com>
2025-07-07 21:12:10 +0300

    add ActivityReportController with userActivityReport method and route

---
+++ app/Http/Controllers/ActivityReportController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use App\Models\InteractiveSignIn;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class ActivityReportController extends Controller
+{
+    public function userActivityReport(Request $request)
+    {
+        $startDate = $request->has('date')
+            ? Carbon::parse($request->input('date'))->startOfDay()
+            : now()->subDays(30);
+
+        $endDate = $request->has('date')
+            ? Carbon::parse($request->input('date'))->endOfDay()
+            : now();
+
+        $activity = User::select([
+                'users.id',
+                'users.displayName',
+                'users.userPrincipalName',
+                DB::raw('COUNT(interactive_sign_ins.id) AS login_count'),
+                DB::raw('MIN(interactive_sign_ins.date_utc) AS first_login'),
+                DB::raw('MAX(interactive_sign_ins.date_utc) AS last_login')
+            ])
+            ->leftJoin('interactive_sign_ins', function ($join) use ($startDate, $endDate) {
+                $join->on('interactive_sign_ins.user_id', '=', 'users.id')
+                     ->whereBetween('interactive_sign_ins.date_utc', [$startDate, $endDate]);
+            })
+            ->groupBy('users.id', 'users.displayName', 'users.userPrincipalName')
+            ->orderByDesc('login_count')
+            ->get();
+
+        return view('activity_report.index', compact('activity', 'startDate', 'endDate'));
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -5,12 +5,16 @@ use App\Http\Controllers\ImportController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ActivityReportController;
+
 
 Route::post('/import-users', [ImportController::class, 'importUsers'])->name('import.users');
 Route::post('/import-sign-ins', [ImportController::class, 'importSignIns'])->name('import.sign_ins');
 
 Route::get('/dashboard', [UserController::class, 'index'])->name('dashboard');
 Route::resource('users', UserController::class);
+Route::get('/activity-report', [ActivityReportController::class, 'userActivityReport'])->name('activity.report');
+
 
 Route::get('/imports', function () {
     return view('imports.index');
