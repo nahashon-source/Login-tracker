296c3d0b229230f93a32793be21efce24d0af945
nahashon <nashonmwendwa0@gmail.com>
2025-07-08 09:12:21 +0300

    fix(routes): redirect root URL to dashboard and organize route definitions cleanly

--- routes/web.php
+++ routes/web.php
@@ -1,29 +1,35 @@
-<?php 
-
+<?php
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ImportController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\ActivityReportController;
 
-// Dashboard + Core
+// Redirect root URL to dashboard
+Route::get('/', function () {
+    return redirect()->route('dashboard');
+});
+
+// Dashboard route
 Route::get('/dashboard', [UserController::class, 'index'])->name('dashboard');
 
-// Logged-in / Not logged-in user lists
+// Logged-in users list
 Route::get('/users/logged-in', [UserController::class, 'loggedInUsers'])->name('users.logged-in');
+
+// Not logged-in users list
 Route::get('/users/not-logged-in', [UserController::class, 'notLoggedInUsers'])->name('users.not-logged-in');
 
-// Users Resource (must come after specific routes)
-Route::resource('users', UserController::class);
-
-// Activity Report
+// User Activity Report
 Route::get('/activity-report', [ActivityReportController::class, 'userActivityReport'])->name('activity.report');
 
-// Imports View
+// Imports view page
 Route::get('/imports', function () {
     return view('imports.index');
 })->name('imports.index');
 
-// CSV Import Endpoints
+// CSV import endpoints
 Route::post('/import-users', [ImportController::class, 'importUsers'])->name('import.users');
 Route::post('/import-sign-ins', [ImportController::class, 'importSignIns'])->name('import.sign_ins');
+
+// User management resource routes (show, create, edit, delete, etc.)
+Route::resource('users', UserController::class);
